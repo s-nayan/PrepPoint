295cffc553607b7e5d9a6ac631f8a457467febfc
s-nayan <nayansaket3@gmail.com>
2025-07-14 19:07:00 +0530

    changes in env

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: process.env.CLIENT_APIKEYS,
-  authDomain: process.env.CLIENT_AUTH_DOMAIN,
-  projectId: process.env.CLIENT_PROJECT_ID,
-  storageBucket: process.env.CLIENT_STORAGE_BUCKET,
-  messagingSenderId: process.env.CLIENT_MESSAGING_SENDER_ID,
-  appId: process.env.CLIENT_APP_ID,
-  measurementId: process.env.CLIENT_MEASUREMENT_ID
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
